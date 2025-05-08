39473dceda04bff97bbd4f358d9201aebc863211
Song1524 <zsdx1525@gmail.com>
2025-05-08 21:21:59 +0900

    0508

--- src/main/resources/static/js/create-vote.js
+++ src/main/resources/static/js/create-vote.js
@@ -110,7 +110,7 @@ document.getElementById("vote-form").addEventListener("submit", async (e) => {
     }
 
     try {
-        const response = await fetch("http://localhost:8080/api/votes", {
+        const response = await fetch("https://votingv2-nvc-production.up.railway.app/api/votes", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
